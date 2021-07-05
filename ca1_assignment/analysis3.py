f82128bbd62c23aae98ded137227c3d762161319
Kelvin Yap <kelvin_yap0306@yahoo.com>
2021-07-05 14:36:07 +0800

    add percentage count to parking system.

--- ca1_assignment/analysis3.py
+++ ca1_assignment/analysis3.py
@@ -2,6 +2,14 @@ from matplotlib.lines import Line2D
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def calculate_parking_system_distribution(coupon_count, electronic_count):
+    total_count = coupon_count + electronic_count
+    coupon_percentage = np.round((coupon_count / total_count) * 100, 2)
+    electronic_percentage = np.round((electronic_count / total_count) * 100, 2)
+    return coupon_percentage, electronic_percentage
+
+
 csv_data = np.genfromtxt("data/hdb-carpark-information.csv",
                          delimiter=",",
                          usecols=(2, 3, 4, 5),
@@ -11,7 +19,8 @@ csv_data = np.genfromtxt("data/hdb-carpark-information.csv",
                                 ('type_of_parking_system', 'U30')])
 
 data_coupon = csv_data[np.isin(csv_data['type_of_parking_system'], ['COUPON PARKING'])]
-data_electric = csv_data[np.isin(csv_data['type_of_parking_system'], ['ELECTRONIC PARKING'])]
+data_electronic = csv_data[np.isin(csv_data['type_of_parking_system'], ['ELECTRONIC PARKING'])]
+perc_coupon , perc_electronic = calculate_parking_system_distribution(len(data_coupon), len(data_electronic))
 
 singapore_img = plt.imread('data/sg_map.png')
 plt.imshow(singapore_img, extent=[919.05, 54338.72, 12576.34, 50172.05], alpha=0.5)
@@ -21,13 +30,13 @@ plt.title("Different Type of Parking System in HDB Singapore")
 # plt.xlabel("Longitude", fontsize=10)
 # plt.ylim(12576.34, 50172.05)
 # plt.xlim(919.05, 54338.72)
-legend_elements = [Line2D([0], [0], marker='X', color='w', label='Coupon', markerfacecolor='r', markersize=10),
-                   Line2D([0], [0], marker='o', color='w', label='Electronic', markerfacecolor='b', markersize=10)]
+legend_elements = [Line2D([0], [0], marker='X', color='w', label="Coupon ({}%)".format(perc_coupon), markerfacecolor='r', markersize=10),
+                   Line2D([0], [0], marker='o', color='w', label="Electronic ({}%)".format(perc_electronic), markerfacecolor='b', markersize=10)]
 plt.legend(handles=legend_elements, loc='upper left', title='Type of Parking')
 plt.axis('off')
 
 plt.scatter(data_coupon['x_coord'], data_coupon['y_coord'], marker='x', color='red', alpha=0.6)
-plt.scatter(data_electric['x_coord'], data_electric['y_coord'], marker='.', color='blue', alpha=0.6)
+plt.scatter(data_electronic['x_coord'], data_electronic['y_coord'], marker='.', color='blue', alpha=0.6)
 
 
 plt.show()
